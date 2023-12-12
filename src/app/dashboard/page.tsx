ae7399967afc5aedce11204998e5365d6dcfceca
sandygudie <goodnewssandy@gmail.com>
2023-12-12 23:22:08 +0100

    fixed deployment

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,16 +3,16 @@ import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import React, { Suspense, useContext, useEffect } from "react";
 import { AppContext } from "@/context";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import Profile from "@/components/Profile";
 import Loading from "../loading";
 
 export default function Dashboard() {
   const { state } = useContext(AppContext);
-  const router = useRouter();
+  // const router = useRouter();
   useEffect(() => {
     if (!(state.userWeb5data.userDid && state.userWeb5data.web5)) {
-      router.push("/");
+      // router.push("/");
     }
   }, []);
 console.log(state)
