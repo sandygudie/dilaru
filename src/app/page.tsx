56d623e747911564bfba5e962d79cfcf5e23aa73
sandygudie <goodnewssandy@gmail.com>
2023-12-13 10:02:41 +0100

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, ChangeEvent, FormEvent, useContext, useEffect } from "react";
+import { useState, ChangeEvent, FormEvent, useContext } from "react";
 import { useRouter } from "next/navigation";
 import { AppContext } from "@/context";
 import { useWeb5 } from "@/hooks/useWeb5";
@@ -13,8 +13,6 @@ export default function Login() {
   const [userName, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-
-  // useEffect(() => {}, [web5, userDid]);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -35,9 +33,9 @@ export default function Login() {
       // });
 
       // if (record) {
+
       // }
-      // console.log(userName);
-      setLoading(false);
+      setLoading(true);
       router.push("/dashboard");
     } catch (error) {
       setLoading(false);
@@ -51,43 +49,35 @@ export default function Login() {
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
-console.log(userDid )
   return (
     <main className="flex min-h-screen bg-black flex-col items-center justify-center">
-      {userDid ? (
-        <form className="" onSubmit={(e) => handleSubmit(e)}>
-          <input
-            onChange={(e) => handleChange(e)}
-            type="text"
-            className="p-4 block mb-6 rounded-lg text-black w-2/6 min-w-[300px]"
-            placeholder="Name"
-            required
-          />
-          <input
-            onChange={(e) => handlePasswordChange(e)}
-            type="password"
-            className="p-4 rounded-lg text-black w-2/6 min-w-[300px]"
-            placeholder="PIN"
-            required
-          />
-          <p className="text-xs mt-2 text-white">
-            (*Your pin is important for account recovery)
-          </p>
-          <div className="text-center w-40 mx-auto">
-            <button
-              type={"submit"}
-              className="p-4 font-bold text-white rounded-lg w-full h-12 my-8 bg-yellow flex flex-col items-center justify-center"
-            >
-              {loading ? <Spinner /> : "Login"}
-            </button>
-          </div>
-        </form>
-      ) : (
-        <div className="text-center">
-          <Spinner />
-          <p className="text-white text-xl mt-2">Retriving data ...</p>
+      <form className="" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          onChange={(e) => handleChange(e)}
+          type="text"
+          className="p-4 block mb-6 rounded-lg text-black w-2/6 min-w-[300px]"
+          placeholder="Name"
+          required
+        />
+        <input
+          onChange={(e) => handlePasswordChange(e)}
+          type="password"
+          className="p-4 rounded-lg text-black w-2/6 min-w-[300px]"
+          placeholder="PIN"
+          required
+        />
+        <p className="text-xs mt-2 text-white">
+          (*Your pin is important for account recovery)
+        </p>
+        <div className="text-center w-40 mx-auto">
+          <button
+            type={"submit"}
+            className="p-4 font-bold text-white rounded-lg w-full h-12 my-8 bg-yellow flex flex-col items-center justify-center"
+          >
+            {loading ? <Spinner /> : "Login"}
+          </button>
         </div>
-      )}
+      </form>
     </main>
   );
 }
