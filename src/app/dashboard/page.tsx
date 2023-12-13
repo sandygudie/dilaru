71706b4f880de614aefc32a856fa32f464f13e2f
sandygudie <goodnewssandy@gmail.com>
2023-12-13 10:45:35 +0100

    fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,24 +6,31 @@ import { AppContext } from "@/context";
 import { useRouter } from "next/navigation";
 import Profile from "@/components/Profile";
 import Loading from "../loading";
+import Spinner from "@/components/Spinner";
 
 export default function Dashboard() {
   const { state } = useContext(AppContext);
   // const router = useRouter();
-// console.log(state)
+  // console.log(state)
   // useEffect(() => {
   //   if (!(state.userWeb5data.userDid && state.userWeb5data.web5)) {
   //     window.location.assign("/");
   //   }
   // }, [state.userWeb5data.userDid, state.userWeb5data.web5]);
-
+  console.log(state);
   return (
-    <div className="flex bg-white items-start relative">
-      <Sidebar />
-      <Header state={state} />
-      <Suspense fallback={<Loading />}>
-        <Profile />
-      </Suspense>
-    </div>
+    <>
+      {state.userWeb5data.userDid ? (
+        <div className="flex bg-white items-start relative">
+          <Sidebar />
+          <Header state={state} />
+          {/* <Suspense fallback={<Loading />}> */}
+            <Profile />
+          {/* </Suspense> */}
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
